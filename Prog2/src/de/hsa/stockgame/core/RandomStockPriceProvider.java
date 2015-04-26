dd636ab7e6b776cb24710fe3c647d02314a55ad5
die-fi <dieter.fischer@hs-augsburg.de>
2015-04-26 18:56:46 +0200

    Update RandomStockPriceProvider.java
    Ändert den Preis

--- Prog2/src/de/hsa/stockgame/core/RandomStockPriceProvider.java
+++ Prog2/src/de/hsa/stockgame/core/RandomStockPriceProvider.java
@@ -3,19 +3,21 @@ package de.hsa.stockgame.core;
 import java.util.Random;
 
 public class RandomStockPriceProvider extends StockPriceProvider {
-	
+	final long MAX_CHANGE = 20;					// Bestimmt die Änderungsspanne des Preises
+									// BSP.: MAX_CHANGE=20 geht von -10 bis +10 
 	Random rand=new Random();
 	
 	public RandomStockPriceProvider() {
 		System.out.println("ShareArray bei rspp an:"+getShares());
 	}
 
-	@Override
+	@Override	
 	public void updateShareRate(Share share) {
-		// TODO Auto-generated method stub
-//		Preis der Aktie zufällig verändern
-//		((max-min)+1)+min
-		share.setPrice((long)rand.nextInt((10-1)+1)+1);
+		long change= -MAX_CHANGE/2 + rand.nextInt((int)MAX_CHANGE);
+		
+		if (share.getPrice() + change >= 0) {			// Prüft ob der Aktienpreis nach der Änderung in negative gehen würde.
+			share.setPrice(share.getPrice() + change);
+		}
 	}
 
 }
