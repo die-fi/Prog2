16ad9bba91e735a2b660205caef91b9d43f0542f
die-fi <dieter.fischer@hs-augsburg.de>
2015-04-26 20:43:12 +0200

    Update StockPriceInfo.java
    Info für Parameterübergaben hinzugefügt

--- Prog2/src/de/hsa/stockgame/core/StockPriceInfo.java
+++ Prog2/src/de/hsa/stockgame/core/StockPriceInfo.java
@@ -4,10 +4,25 @@ import de.hsa.stockgame.exception.ShareNotExistsException;
 
 public interface StockPriceInfo {
 	
+	/**
+	 * 
+	 * @param String - Name der Gesuchten Aktie
+	 * @return boolean true = Aktie existiert, false existiert nicht
+	 */	
 	public boolean isShareListed(String shareName);
 	
+	
+	/**
+	 * Gibt den aktuellen Kurs der Aktie wieder.
+	 * @param share
+	 * @return long - Preis der Aktie
+	 */	
 	public long getCurrentShareRate(String shareName) throws ShareNotExistsException;
 	
+	/**
+	 * Gibt alle im Spiel vorhandene Aktien und deren Kurz als Text aus
+	 * @return Share[] - Wiedergabe aller Share in einem ShareArray
+	 */	
 	public Share[] getAllSharesAsSnapShot();
 
 }
